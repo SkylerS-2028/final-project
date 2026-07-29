1c8e2acbb7dd33745ec7f9387cc8ad2ac4521819
Skyler Stevenson <skylerstevenson@icloud.com>
2026-07-29 15:28:59 -0700

    Completed updated hours feature

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import './ToDoList.css';
 
 /*
@@ -7,14 +8,15 @@ Features to add:
 - Hours of productivity 
 */
 
+
 function ToDoList(){
     
     // Task information
     const [tasks, setTasks] = useState(
         {
-            title:["Final Project"],
-            description:["This is your opportunity to showcase what you’ve learned and to stretch your creativity and problem solving skills. Your project should reflect your understanding of modern JavaScript programming and its integration into the web development ecosystem."], 
-            due:["2026-7-31"],
+            title:["Final Project", "Sample Task"],
+            description:["This is your opportunity to showcase what you’ve learned and to stretch your creativity and problem solving skills. Your project should reflect your understanding of modern JavaScript programming and its integration into the web development ecosystem.", "Sample task description"], 
+            due:["2026-7-31", "2026-09-13"],
             hours:[0]
         }
     );
@@ -23,6 +25,7 @@ function ToDoList(){
     const [newTask, setNewTask] = useState ("");
     const [newDescr, setNewDescr] = useState("");
     const [newDue, setNewDue] = useState("");
+    const [newHour, setNewHour] = useState(0);
 
     // Productivity information
     const [completed, setCompleted] = useState(0);
@@ -30,55 +33,71 @@ function ToDoList(){
     //const [progressHours, setProgressHours] = useState(0);
  
     // Functions
-        function changeHours(){
-            console.log(tasks);
-            setTasks()
+    function changeHours(index){
+        if (newHour.trim() !== "" && newHour !== 0){
+            console.log(newHour);
+            const updatedHours = [...tasks.hours];
+            updatedHours[index] = [newHour];
+            console.log(updatedHours);
+            setTasks(prev => ({
+                ...prev,
+                hours: updatedHours,
+            }))
         }
+        setNewHour(0)
+    }
 
-        function addTask(){
-            if(newTask.trim() !== "" && newDescr.trim() !== "" && newDue !== ""){
+    function addTask(){
+        if(newTask.trim() !== "" && newDescr.trim() !== "" && newDue !== ""){
 
-                setTasks(prev => ({
-                    title: [...prev.title, newTask],
-                    description: [...prev.description, newDescr],
-                    due:[...prev.due, newDue],
-                    hours: [...prev.hours, 0]
-                }));
-                setNewTask("");
-                setNewDescr("");
-                setNewDue("");
+            setTasks(prev => ({
+                title: [...prev.title, newTask],
+                description: [...prev.description, newDescr],
+                due:[...prev.due, newDue],
+                hours: [...prev.hours, 0]
+            }));
+
+            setNewTask("");
+            setNewDescr("");
+            setNewDue("");
+
+            console.log(tasks)
 
             }
-        }
+    }
 
-        function deleteItem(index){
+    function deleteItem(index){
 
-            const updatedTasks = tasks.title.filter((element, i) => i !== index);
-            const updatedDescription = tasks.description.filter((element, i) => i !== index);
-            const updatedDates = tasks.due.filter((element, i) => i !== index);
+        const updatedTasks = tasks.title.filter((element, i) => i !== index);
+        const updatedDescription = tasks.description.filter((element, i) => i !== index);
+        const updatedDates = tasks.due.filter((element, i) => i !== index);
 
-            setTasks({
-                title: updatedTasks,
-                description: updatedDescription,
-                due: updatedDates,
-            });
+        setTasks({
+            title: updatedTasks,
+            description: updatedDescription,
+            due: updatedDates,
+        });
 
-        }
+    }
 
-        function completeItem(index){
+    function completeItem(index){
 
-            const updatedTasks = tasks.title.filter((element, i) => i !== index);
-            const updatedDescription = tasks.description.filter((element, i) => i !== index);
-            const updatedDates = tasks.due.filter((element, i) => i !== index);
+        const updatedTasks = tasks.title.filter((element, i) => i !== index);
+        const updatedDescription = tasks.description.filter((element, i) => i !== index);
+        const updatedDates = tasks.due.filter((element, i) => i !== index);
 
-            setTasks({
-                title: updatedTasks,
-                description: updatedDescription,
-                due: updatedDates,
-            });
+        setTasks({
+            title: updatedTasks,
+            description: updatedDescription,
+            due: updatedDates,
+        });
 
-            setCompleted(completed + 1);
-        }
+        setCompleted(completed + 1);
+    }
+
+    useEffect(() => {
+        console.log(tasks);
+    }, [tasks])
 
     // HTML
     return(  
@@ -112,10 +131,12 @@ function ToDoList(){
                 />
 
                 <button id="add-button" onClick={addTask}>Add</button>
+
             </div>
 
             <ol>
                 {tasks.title.map((task, index) => 
+
                 <li className='list-item' key={index}>
                     <div className='task-content'>
                         <p style={{ color: 'black', fontWeight: 'bold', paddingTop: '20px' }}>{task}</p>
@@ -125,14 +146,19 @@ function ToDoList(){
                         type="number"
                         placeholder="Current hours"
                         id="hours"
-                        onChange={changeHours}
+                        onChange={(e) => { 
+                            setNewHour(e.target.value);
+                        }}
                         />
+                        <button onClick={() => changeHours(index)}>Update Hours</button>
                     </div>
                     <div className='remove-buttons'>
                         <button id='delete-task' onClick={() => deleteItem(index)}>✗</button>
                         <button id='complete-task' onClick={() => completeItem(index)}>✓</button>
                     </div>
-                </li>)}
+                </li>
+            
+                )}
             </ol>
         </div>
     );
